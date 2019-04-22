779c2eebebedc35ee918f20ddf96fbd7cb79c0b8
Edward Chopuryan <echopuryan@foundationmedicine.com>
2019-04-22 15:20:11 -0400

    added models and implmented detection

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,12 @@ class App extends Component {
     super(props);
 
     this.videoRef = undefined;
+    this.imgRef = undefined;
     this.constraints = { audio: false, video: { width: 1280, height: 720 } };
 
     this.state = {
-      detectionsWithExpressions: undefined
+      detectionsWithExpressions: undefined,
+      bestPrediction: undefined
     };
   }
 
@@ -18,23 +20,50 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <video ref={ref => (this.videoRef = ref)} autoPlay />
-          <div />
+          <div className="video">
+            <video ref={ref => (this.videoRef = ref)} autoPlay />
+          </div>
+          <div>
+            {this.state.detectionsWithExpressions && (
+              <div>
+                <span>
+                  {`Expression: ${
+                    this.state.detectionsWithExpressions.expressions[0]
+                      .expression
+                  } - ${(
+                    this.state.detectionsWithExpressions.expressions[0]
+                      .probability * 100
+                  ).toFixed(2)} %`}
+                </span>
+              </div>
+            )}
+          </div>
         </header>
       </div>
     );
   }
 
   async componentDidMount() {
-    await faceapi.nets.ssdMobilenetv1.loadFromDisk("../models");
+    await faceapi.loadSsdMobilenetv1Model("/models");
+    await faceapi.loadFaceExpressionModel("/models");
     await this.getStreamFromCamera();
-    await this.detectFaces();
+    setTimeout(this.detectFaces, 1500);
   }
 
+  sortPredictions = (a, b) => {
+    if (a.probability > b.probability) return -1;
+    if (b.probability > a.probability) return 1;
+    return 0;
+  };
+
   detectFaces = async () => {
+    console.log("detecting faces...");
     const detectionsWithExpressions = await faceapi
       .detectSingleFace(this.videoRef)
       .withFaceExpressions();
+    detectionsWithExpressions.expressions = detectionsWithExpressions.expressions.sort(
+      this.sortPredictions
+    );
     this.setState({ detectionsWithExpressions });
     console.log(detectionsWithExpressions);
   };
